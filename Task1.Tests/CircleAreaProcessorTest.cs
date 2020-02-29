bf2c56b5506e7964413d7e4d17204790cae9e62a
Romeo <rrv.kazan@gmail.com>
2020-02-29 17:56:46 +0300

    feat: add tests

---
+++ Task1.Tests/CircleAreaProcessorTest.cs
@@ -0,0 +1,26 @@
+namespace Task1.Tests
+{
+    using System;
+    using Task1.Processors;
+    using Task1.Processors.CircleArea;
+    using Xunit;
+
+    public class CircleAreaProcessorTest
+    {
+        private readonly ICircleAreaProcessor processor;
+
+        public CircleAreaProcessorTest()
+        {
+            this.processor = new CircleAreaProcessor();
+        }
+
+        [Theory]
+        [InlineData(10)]
+        public void PassingTest(double radius)
+        {
+            var expected = Math.PI * radius * radius;
+            var result = this.processor.Execute(radius);
+            Assert.Equal(result, expected);
+        }
+    }
+}
---
+++ Task1.Tests/RightTriangleProcessorTest.cs
@@ -0,0 +1,28 @@
+namespace Task1.Tests
+{
+    using System;
+    using Task1.Processors;
+    using Task1.Processors.TriangleArea;
+    using Xunit;
+
+    public class TriangleAreaProcessorTest
+    {
+        private readonly ITriangleAreaProcessor processor;
+
+        public TriangleAreaProcessorTest()
+        {
+            this.processor = new TriangleAreaProcessor();
+        }
+
+        [Theory]
+        [InlineData(3, 4, 5)]
+        public void PassingTest(double a, double b, double c)
+        {
+            var p = (a + b + c) / 2;
+            var expected = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+            var result = this.processor.Execute(a, b, c);
+            Assert.Equal(result, expected);
+        }
+    }
+}
